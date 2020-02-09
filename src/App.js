abece6e67868c9ac70a84dd101c5bde277a6be55
VikaNilha <vik825@ukr.net>
2020-02-09 13:20:30 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from './pages/Auth';
+
+function App() {
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
